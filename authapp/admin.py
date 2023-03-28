329c7359c0bc8a5b2c311d7915e18c3cd9a0b452
donalte <donaldtedom0@gmail.com>
2023-03-28 23:00:05 +0100

    fix: fixing massage table and design

--- authapp/admin.py
+++ authapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.site_title = "Django Chat"
+admin.site.site_header = "Django Chat"
+admin.site.index_title = "DJango Chat"
--- room/admin.py
+++ room/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Room
+from .models import Room, Message
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
     list_display = ('name','id')
+
+admin.site.register(Message)    
